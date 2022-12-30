40c85d34a1f1e91c5094043a3bf71ee0ed315efe
arthurdiasdeveloper <arthurdiascorporativo@gmail.com>
2022-12-30 00:21:07 -0300

    second commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+    
+        
+        <h1>
+        Parabéns, rendeirzou!
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button/index.js
@@ -0,0 +1,9 @@
+import styles from './styles'
+
+const Button = () => {
+    return ( 
+        <button className = "Button"> </button>
+        )
+};
+
+export default Button
